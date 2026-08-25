da7bdff02ce9c10a00444f889fd7f37a396f945c
buildwithutsav <maharishiutsav@gmail.com>
2026-08-25 21:27:50 +0530

    stack pgrm in c using dynamic size array

--- stack.1.c
+++ stack.1.c
@@ -1,6 +1,6 @@
 //stack program in c using fixed size array
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 int top=-1 ,max=3 , arr[4];
 int main(){
@@ -48,6 +48,70 @@ int pop()
 
     de=arr[top];
 
+    top=top--;
+    return de;
+}*/
+
+
+//stack program in c using dynamic size array
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int top=-1 , max , *arr;
+int main(){
+    int n,i,de;
+    void push(int);
+    int pop(void);
+
+    printf("enter size of array");
+    scanf("&d",&n);
+    max=n-1;
+    arr = malloc(sizeof(int)*n);
+
+    push(1);
+    push(2);
+    push(3);
+
+
+    de=pop();
+    printf("popped element &d\n",de);
+    printf("current top = &d\n",top);
+
+    de=pop();
+    printf("popped element &d\n",de);
+    printf("current top = &d\n",top);
+
+    de=pop();
+    printf("popped element &d\n",de);
+    printf("current top = &d\n",top);
+
+    for(i=0;i<=top;i++)
+    printf("%d",arr[i]);
+
+    return 0;
+}
+
+void push(int e)
+{
+    if(top==max)
+    {
+        printf("overflow");
+        exit(1);
+    }
+    top=top++;
+    arr[top]=e;
+}
+
+int pop()
+{
+    int de;
+    if(top==-1)
+    {
+        printf("under flow");
+        exit(1);
+    }
+    de=arr[top];
     top=top--;
     return de;
 }
