e1b36dbc0ef20b16383f351a66a349e5670b8d33
buildwithutsav <maharishiutsav@gmail.com>
2026-08-31 21:22:46 +0530

    founded missing numbers

---
+++ find_missing_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    int n,i;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    int arr[n-1];
+
+    printf("Enter %d elements: \n", n-1);
+    for(i=0;i<n-1;i++){
+        scanf("%d",&arr[i]);
+
+
+    }
+
+    int total = n*(n+1)/2;
+    int sum = 0;
+
+    for (i=0;i<n-1;i++){
+        sum = sum +arr[i];
+
+    }
+
+    int missing = total - sum;
+    printf("missing number = %d ,missing");
+
+    return 0;
+}
