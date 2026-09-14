f6cfa92aa359c05fb0cf3f6926f0859abdd5638f
buildwithutsav <maharishiutsav@gmail.com>
2026-09-14 22:37:24 +0530

    Delete a Node from Beginning

---
+++ delete_a_node_from_begining.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+void insertEnd(int value) {
+
+    struct Node *newNode =
+        (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    struct Node *temp = head;
+
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+void deleteBeginning() {
+
+    if(head == NULL) {
+
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node *temp = head;
+
+    head = head->next;
+
+    free(temp);
+}
+
+void display() {
+
+    struct Node *temp = head;
+
+    while(temp != NULL) {
+
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+
+    insertEnd(10);
+    insertEnd(20);
+    insertEnd(30);
+    insertEnd(40);
+
+    display();
+
+    deleteBeginning();
+
+    display();
+
+    return 0;
+}
