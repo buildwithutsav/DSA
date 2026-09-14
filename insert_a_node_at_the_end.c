164b5d8550a2c6dd3c863d09c908cf54dac7c7bd
buildwithutsav <maharishiutsav@gmail.com>
2026-09-14 22:34:19 +0530

    Insert a Node at the End

---
+++ insert_a_node_at_the_end.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+void insertEnd(int value) {
+
+    struct Node *newNode;
+    struct Node *temp;
+
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(head == NULL) {
+
+        head = newNode;
+        return;
+    }
+
+    temp = head;
+
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+void display() {
+
+    struct Node *temp = head;
+
+    while(temp != NULL) {
+
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+
+    insertEnd(10);
+    insertEnd(20);
+    insertEnd(30);
+    insertEnd(40);
+
+    display();
+
+    return 0;
+}
